134e100292659f90aa91fda8067104ca5dad11d7
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-04-14 09:37:50 +0530

    create a CurrentWeather Component and attach it to the dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather";
+import CurrentWeather from "@/components/current-weather";
 
 const WeatherDashboard = () => {
   const {
@@ -12,12 +14,18 @@ const WeatherDashboard = () => {
     isLoading: locationLoading,
   } = useGeolocation();
 
-  console.log(coordinates);
+const locationQuery = useReverseGeocodeQuery(coordinates);
+const weatherQuery = useWeatherQuery(coordinates);
+const forecastQuery = useForecastQuery(coordinates);
+
+console.log(weatherQuery.data)
 
   const handleRefresh = () => {
     getLocation();
     if (coordinates) {
-      //reload weather data
+      weatherQuery.refetch();
+      forecastQuery.refetch();
+      locationQuery.refetch();
     }
   };
 
@@ -52,14 +60,49 @@ const WeatherDashboard = () => {
     </Alert>;
   }
 
+  const locationName = locationQuery.data?.[0];
+
+  if(weatherQuery.error || forecastQuery.error){
+    return <Alert variant="destructive">
+    <AlertTriangle className="h-4 w-4" />
+    <AlertTitle>Error</AlertTitle>
+    <AlertDescription className="flex flex-col gap-4">
+      <p>Failed to fetch weather data. Please try again</p>
+      <Button onClick={handleRefresh} variant={"outline"} className="w-fit" >
+        <RefreshCw className="mr-2 h-4 w-4" />
+        Retry
+      </Button>
+    </AlertDescription>
+  </Alert>;
+  }
+
+  if(!weatherQuery.data || !forecastQuery.data){
+    return <WeatherSkeleton/>;
+  }
+
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
         <h1 className="text-xl font-bold tracking-tight">My Location</h1>
-        <Button variant={"outline"} size={"icon"} onClick={handleRefresh}>
-          <RefreshCw className="h-4 w-4" />
+        <Button variant={"outline"} size={"icon"} onClick={handleRefresh} disabled={weatherQuery.isFetching || forecastQuery.isFetching}>
+          <RefreshCw className={`h-4 w-4 ${weatherQuery.isFetching ? "animate-spin" : ""}`} />
         </Button>
       </div>
+
+      <div className="grid gap-6">
+        <div>
+          <CurrentWeather
+          data={weatherQuery.data}
+          locationName={locationName}
+          />
+
+          
+        </div>
+
+        <div>
+        </div>
+      </div>
+
     </div>
   );
 };
